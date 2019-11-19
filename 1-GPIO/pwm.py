256cbcbb8b682d2937c642f84432df1f2a87dc6d
EteriumD <Lautaroandressaez@gmail.com>
2019-11-19 17:59:54 -0300

    Se realizo el ejercicio 1

--- 1-GPIO/pwm.py
+++ 1-GPIO/pwm.py
@@ -4,10 +4,21 @@ import time
 
 GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
-pwm = GPIO.PWM(12, 1000)
-pwm.start(30)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(28, GPIO.OUT)
+
+GPIO.setup(21, GPIO.IN)
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(25, GPIO.IN)
+GPIO.setup(27, GPIO.IN)
 
 
 while True:
-    pass
+
+    Entrada = GPIO.wait_for_edge(21)
+
+    if Entrada==True:
+        GPIO.output(22,True)
+        
