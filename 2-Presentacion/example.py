71ad0c521ce09f87662fc5b029720ecc65aca590
EteriumD <Lautaroandressaez@gmail.com>
2019-11-25 19:51:01 -0300

    se incluyeron las funciones basicas del modulo window

--- 2-Presentacion/example.py
+++ 2-Presentacion/example.py
@@ -1,6 +1,8 @@
 import oled 
 import display
 import font
+import window
+import time.sleep as delay
 
 displayOled = oled.DisplayOled()
 
@@ -11,12 +13,24 @@ displayOled.iniciar_modulo()
 display = [0 for x in range(1024)]
 
 fontVar = font.FontOled()
+win = window.Window()
 
-ubicaciones = fontVar.get_frase('Hola pibe! @')
+win.cargar_elemento(fontVar, 'flecha_arriba', [8,0])
+
+win.cargar_elemento(fontVar, 'H', [0,0])
+
+ubicaciones = fontVar.get_frase('Hola Buenas tardes!')
+
 
 for j in range(len(ubicaciones)):
     letra = ubicaciones[j]
     for i in range(len(letra)):
         display[i + 8*j] = letra[i]
 
+display_2 = display.window_to_display(win.window)
+
 displayOled.cargar_display( display )
+
+delay(1)
+
+displayOled.cargar_display( display_2 )
--- 2-Presentacion/window.py
+++ 2-Presentacion/window.py
@@ -1,11 +1,36 @@
+import warnings as alert
+
 class Window:
 
     def __init__(self):
         self.window = [0 for i in range(1024)]
         
 
-    def cargar_elemento(self, elemento, bit_fila_inicial, bit_columna_inicial):
-        dimensiones = len( elemento )
+    def cargar_elemento(self, font, element_name, bit_inicial=[0, 0]):
+        element = self.buscar_elemento(font, element_name)
+        self.cargar_figura(element, bit_inicial)
+        
+    
+    def buscar_elemento(self, font, element_name):
+        if(type(font) != 'FontOled'):
+            alert.warn('Se espera recibir una variable tipo Font')
+        
+        font_elements_names = font.letras
+        existe = False
+        for i in range(len(font_elements_names)):
+            if font_elements_names[i] == element_name:
+                existe = True
+                break
+        
+        if ~existe:
+            alert.warn('No se encontro el elemento esperado!')
+        
+        return font.get_letra(i)
+
+    def cargar_figura(self, figure, bit_inicial):
+        dimensiones = len( figure )
         for i in range( dimensiones[0] ):
             for j in range( dimensiones[1] ):
-                self.window[i + bit_fila_inicial][j + bit_columna_inicial] = elemento[i][j]
+                self.window[i + bit_inicial[0]][j + bit_inicial[1]] = figure[i][j]
+        
+    
