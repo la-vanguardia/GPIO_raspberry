4d5d876c6ebbbd1bfe22722a876feadab97c9dc7
EteriumD <Lautaroandressaez@gmail.com>
2019-11-24 22:31:51 -0300

    fontOled update

--- 2-Presentacion/font.py
+++ 2-Presentacion/font.py
@@ -6,8 +6,13 @@ class FontOled:
     def __init__(self, path = './Resource/Images/atascii.bmp'):
         self.font_matriz = display.bmp_to_display(path)
 
-    def letra(self, letra):
-        return self.font_matriz[ letra ]
+    def get_letra(self, letra):
+        letraOled = []
+        for i in range(8):
+            for j in range(8):
+                letraOled.append( self.font_matriz[letra + i +j] )
+
+        return letraOled
 
         
 
